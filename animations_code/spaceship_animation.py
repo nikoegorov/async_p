ab8108ae6107d6438254aabdea48dbb466319d73
Niko <nikoegorov@users.noreply.github.com>
2019-04-08 04:29:48 +0300

    WIP - at 13/14 - compare two approaches in spaceship_animation.py - commented and uncommented

--- animations_code/spaceship_animation.py
+++ animations_code/spaceship_animation.py
@@ -10,10 +10,10 @@ async def animate_spaceship(
     canvas,
     start_row: int,
     start_column: int,
-    border_limit_rows_top: int,
-    border_limit_rows_bottom: int,
-    border_limit_columns_left: int,
-    border_limit_columns_right: int,
+    border_limit_top: int,
+    border_limit_bottom: int,
+    border_limit_left: int,
+    border_limit_right: int,
     animations: List[str]
 ):
     animations_cycle = cycle(animations)
@@ -22,48 +22,94 @@ async def animate_spaceship(
     column_shift = 0
 
     while True:
+        # rows_direction, columns_direction, _ = read_controls(canvas)
+        #
+        # filename, frame = next(animations_cycle)
+        # frame_height, frame_width = get_frame_size(frame)
+        #
+        # shift_frame_rows_to_center = -int(frame_height / 2)
+        # shift_frame_columns_to_center = -int(frame_width / 2)
+        #
+        # old_frame_top_row = start_row + row_shift
+        # old_frame_left_column = start_column + column_shift
+        #
+        # new_position_top_row = old_frame_top_row + rows_direction
+        # new_position_left_column = old_frame_left_column + columns_direction
+        #
+        # new_frame_top_row = new_position_top_row + shift_frame_rows_to_center
+        # new_frame_bottom_row = new_position_top_row - shift_frame_rows_to_center
+        #
+        # new_frame_left_column = new_position_left_column + shift_frame_columns_to_center
+        # new_frame_right_column = new_position_left_column - shift_frame_columns_to_center
+        #
+        # if new_frame_top_row < border_limit_top:
+        #     rows_direction = 0
+        #
+        # if new_frame_bottom_row > border_limit_bottom:
+        #     rows_direction = 0
+        #
+        # if new_frame_left_column < border_limit_left:
+        #     columns_direction = 0
+        #
+        # if new_frame_right_column > border_limit_right:
+        #     columns_direction = 0
+        #
+        # row_shift += rows_direction
+        # column_shift += columns_direction
+        #
+        # spaceship_center_row = start_row + row_shift + shift_frame_rows_to_center
+        # spaceship_center_column = start_column + column_shift + shift_frame_columns_to_center
+        #
+        # draw_frame(canvas, spaceship_center_row, spaceship_center_column, frame)
+        # canvas.refresh()
+        # for i in range(2):
+        #     await asyncio.sleep(0)
+        # draw_frame(canvas, spaceship_center_row, spaceship_center_column, frame,
+        #            negative=True)
+        # canvas.refresh()
+
+
         rows_direction, columns_direction, _ = read_controls(canvas)
 
         filename, frame = next(animations_cycle)
         frame_height, frame_width = get_frame_size(frame)
 
-        shift_frame_rows_to_center = -int(frame_height / 2)
-        shift_frame_columns_to_center = -int(frame_width / 2)
+        frame_center_rows = int(frame_height / 2)
+        frame_center_columns = int(frame_width / 2)
 
-        current_position_row = start_row + row_shift
-        current_position_column = start_column + column_shift
+        old_frame_top_row = start_row + row_shift
+        new_frame_top_row = old_frame_top_row + rows_direction
+        new_frame_bottom_row = new_frame_top_row + frame_height
 
-        desired_position_top_row = current_position_row + rows_direction
-        desired_position_left_column = current_position_column + columns_direction
-
-        desired_frame_top_row = desired_position_top_row + shift_frame_rows_to_center
-        desired_frame_bottom_row = desired_position_top_row - shift_frame_rows_to_center
-
-        desired_frame_left_column = desired_position_left_column + shift_frame_columns_to_center
-        desired_frame_right_column = desired_position_left_column - shift_frame_columns_to_center
-
-        if desired_frame_top_row < border_limit_rows_top:
+        if any([
+            new_frame_top_row < border_limit_top + frame_center_rows,
+            new_frame_bottom_row > border_limit_bottom + frame_center_rows,
+        ]):
             rows_direction = 0
 
-        if desired_frame_bottom_row > border_limit_rows_bottom:
-            rows_direction = 0
+        old_frame_left_column = start_column + column_shift
+        new_frame_left_column = old_frame_left_column + columns_direction
+        new_frame_right_column = new_frame_left_column + frame_width
 
-        if desired_frame_left_column < border_limit_columns_left:
+        if any([
+            new_frame_left_column < border_limit_left + frame_center_columns,
+            new_frame_right_column > border_limit_right + frame_center_columns,
+        ]):
             columns_direction = 0
 
-        if desired_frame_right_column > border_limit_columns_right:
-            columns_direction = 0
+        new_frame_top_row = old_frame_top_row + rows_direction
+        new_frame_left_column = old_frame_left_column + columns_direction
 
-        row_shift += rows_direction
-        column_shift += columns_direction
+        ship_center_position_row = new_frame_top_row - frame_center_rows
+        ship_center_position_column = new_frame_left_column - frame_center_columns
 
-        spaceship_center_row = start_row + row_shift + shift_frame_rows_to_center
-        spaceship_center_column = start_column + column_shift + shift_frame_columns_to_center
-
-        draw_frame(canvas, spaceship_center_row, spaceship_center_column, frame)
+        draw_frame(canvas, ship_center_position_row, ship_center_position_column, frame)
         canvas.refresh()
         for i in range(2):
             await asyncio.sleep(0)
-        draw_frame(canvas, spaceship_center_row, spaceship_center_column, frame,
+        draw_frame(canvas, ship_center_position_row, ship_center_position_column, frame,
                    negative=True)
         canvas.refresh()
+
+        row_shift += rows_direction
+        column_shift += columns_direction
--- main.py
+++ main.py
@@ -9,61 +9,87 @@ from animations_code.load_animations_frames import load_animations_from_folder
 from animations_code.spaceship_animation import animate_spaceship
 
 
-TIC_TIMEOUT = 0.1
+TIC_TIMEOUT = 0.01
+COMPLEX_ANIMATIONS_FOLDER = 'complex_animations'
+SPACESHIP_ANIMATIONS_FOLDER = 'spaceship'
+SPACESHIP_ANIMATIONS = load_animations_from_folder(
+        path.join(COMPLEX_ANIMATIONS_FOLDER, SPACESHIP_ANIMATIONS_FOLDER)
+    )
 
 
-def draw(canvas, spaceship_animations):
+
+def discover_active_area(canvas) -> dict:
+    max_height, max_width = canvas.getmaxyx()
+
+    return {
+        'border_limit_top': 1,
+        'border_limit_left': 1,
+        'border_limit_right': max_width - 1,
+        'border_limit_bottom': max_height - 1,
+        'window_center_row': int(max_height / 2),
+        'window_center_column': int(max_width / 2),
+    }
+
+
+def prepare_blink_coroutines(
+    canvas,
+    amount_of_stars: int=100,
+    stars_symbols="+*.:"
+) -> list:
+    active_area = discover_active_area(canvas)
+
+    return [
+        blink(
+            canvas=canvas,
+            # We don't want to place stars at borderline, so 1 row/column cap
+            row=randint(
+                active_area['border_limit_top'],
+                active_area['border_limit_bottom'] - 1
+            ),
+            column=randint(
+                active_area['border_limit_left'],
+                active_area['border_limit_right'] - 1
+            ),
+            symbol=choice(stars_symbols),
+        )
+        for _ in range(amount_of_stars)
+    ]
+
+
+def prepare_fire_coroutine(canvas):
+    active_area = discover_active_area(canvas)
+    return fire(
+            canvas=canvas,
+            start_row=active_area['window_center_row'],
+            start_column=active_area['window_center_column'],
+        )
+
+
+def prepare_spaceship_animation(canvas, spaceship_animations=SPACESHIP_ANIMATIONS):
+    active_area = discover_active_area(canvas)
+
+    return animate_spaceship(
+        canvas=canvas,
+        start_row=active_area['window_center_row'],
+        start_column=active_area['window_center_column'],
+        border_limit_top=active_area['border_limit_top'],
+        border_limit_bottom=active_area['border_limit_bottom'],
+        border_limit_left=active_area['border_limit_left'],
+        border_limit_right=active_area['border_limit_right'],
+        animations=spaceship_animations,
+    )
+
+
+def draw(canvas):
 
     curses.curs_set(False)
     canvas.nodelay(True)
     canvas.border()
 
-    max_height, max_width = canvas.getmaxyx()
-    canvas.addstr(20, 2, "Your terminal is %dx%d in size." % (max_width, max_height))
-
-    border_limit_top = 1
-    border_limit_left = 1
-    border_limit_right = max_width - 2
-    border_limit_bottom = max_height - 2
-
-    window_center_row = int(border_limit_bottom / 2)
-    window_center_column = int(border_limit_right / 2)
-
-    canvas.refresh()
-    stars_symbols = "+*.:"
-
-    coroutines = [
-        blink(
-            canvas=canvas,
-            # Border takes 1 line at top and 2 lines at bottom.
-            row=randint(1, border_limit_bottom),
-            # Border takes 1 line from the left and 2 lines at the right.
-            column=randint(1, border_limit_right),
-            symbol=choice(stars_symbols),
-        )
-        for _ in range(100)
-    ]
-
-    coroutines.append(
-        fire(
-            canvas=canvas,
-            start_row=window_center_row,
-            start_column=window_center_column,
-        )
-    )
-
-    coroutines.append(
-        animate_spaceship(
-            canvas=canvas,
-            start_row=window_center_row,
-            start_column=window_center_column,
-            border_limit_rows_top=border_limit_top,
-            border_limit_rows_bottom=border_limit_bottom,
-            border_limit_columns_left=border_limit_left,
-            border_limit_columns_right=border_limit_right,
-            animations=spaceship_animations,
-        )
-    )
+    coroutines = []
+    coroutines.extend(prepare_blink_coroutines(canvas))
+    coroutines.append(prepare_fire_coroutine(canvas))
+    coroutines.append(prepare_spaceship_animation(canvas))
 
     while True:
         if len(coroutines) == 0:
@@ -82,14 +108,9 @@ def draw(canvas, spaceship_animations):
 
 def main():
 
-    complex_animations_folder = 'complex_animations'
-    spaceship_animations_folder = 'spaceship'
-    spaceship_animations = load_animations_from_folder(
-        path.join(complex_animations_folder, spaceship_animations_folder)
-    )
 
     curses.update_lines_cols()
-    curses.wrapper(draw, spaceship_animations)
+    curses.wrapper(draw)
     sleep(1)
 
 
